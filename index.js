dd765f634c447579d75fbd03624d12a54b0310f0
Shay Bergmann <shaybergmann@gmail.com>
2018-05-17 12:23:54 -0400

    refactor

--- index.js
+++ index.js
@@ -1,81 +1,52 @@
 const Listr = require('listr')
-const { exec } = require('./lib/exec')
 const { Subject } = require('rxjs/Subject')
 const { Observable } = require('rxjs/Observable')
 const { forkJoin } = require('rxjs/observable/forkJoin')
+const { concat } = require('rxjs/observable/concat')
 const { yellow, green } = require('chalk')
 
-const sleep = d => () => exec('sleep', [d.toString()])
-const sleep5 = sleep(5)
-
-const input = {
-  build: ['gulpBuild'],
-  gulpBuild: ['gulpBuildSrc', 'gulpBuildAssets'],
-  gulpBuildSrc: ['gulpBuildSrcJs', 'gulpBuildSrcCoffee', 'gulpBuildSrcJson'],
-  gulpBuildSrcJs: sleep5,
-  gulpBuildSrcCoffee: sleep5,
-  gulpBuildSrcJson: sleep5,
-  gulpBuildAssets: [
-    'gulpBuildAssetsBrowserify',
-    'gulpBuildAssetsJs',
-    'gulpBuildAssetsCss',
-    'gulpBuildAssetsFingerprint'
-  ],
-  gulpBuildAssetsBrowserify: sleep(1),
-  gulpBuildAssetsJs: sleep(3),
-  gulpBuildAssetsCss: sleep(6),
-  gulpBuildAssetsFingerprint: {
-    dependencies: [
-      'gulpBuildAssetsBrowserify',
-      'gulpBuildAssetsJs',
-      'gulpBuildAssetsCss'
-    ],
-    action: sleep(1)
+class Tusk {
+  constructor (input) {
+    this.input = Object.entries(input).reduce(
+      (a, [key, value]) => ({
+        ...a,
+        [key]: {
+          dependencies: Array.isArray(value)
+            ? value
+            : value.dependencies ? value.dependencies : [],
+          action: typeof value === 'function'
+            ? value
+            : value.action ? value.action : null
+        }
+      }),
+      {}
+    )
   }
-}
 
-const normalizeInput = input =>
-  Object.entries(input).reduce(
-    (a, [key, value]) => ({
-      ...a,
-      [key]: {
-        dependencies: Array.isArray(value)
-          ? value
-          : value.dependencies ? value.dependencies : [],
-        action: typeof value === 'function'
-          ? value
-          : value.action ? value.action : null
+  run (task) {
+    const result = this.expandDependencies(task)
+    const context = result.reduce((acc, { name }) => {
+      const subject = new Subject()
+      subject.completed = false
+      subject.subscribe(null, null, () => (subject.completed = true))
+      return {
+        ...acc,
+        [name]: subject
       }
-    }),
-    {}
-  )
+    }, {})
 
-const normalizedInput = normalizeInput(input)
-
-function build () {
-  const result = expandDependencies('build')
-  const context = result.reduce((acc, { name }) => {
-    const subject = new Subject()
-    subject.next(name)
-    subject.completed = false
-    subject.subscribe(null, null, () => (subject.completed = true))
-    return {
-      ...acc,
-      [name]: subject
-    }
-  }, {})
-
-  return new Listr(
-    result.map(({ name, action, dependencies }) => ({
-      title: name +
+    return new Listr(
+      result.map(({ name, action, dependencies }) => ({
+        title: name +
         (dependencies.length > 0
           ? ` [ ${yellow(dependencies.join(' '))} ]`
           : ''),
-      task: (ctx, task) => {
-        const dependencies$ = dependencies.map(dep => [dep, ctx[dep]])
-        dependencies$.forEach(([dep, dep$]) => {
-          dep$.subscribe(null, null, () => {
-            task.title =
+        task: (ctx, task) => {
+          const dependencies$ = dependencies.map(dep => [dep, ctx[dep]])
+          dependencies$.forEach(([dep, dep$]) => {
+            dep$.next(name)
+            dep$.subscribe(null, null, () => {
+              task.title =
               name +
               (dependencies.length > 0
                 ? ` [ ${dependencies$
@@ -84,40 +55,34 @@ function build () {
                   )
                   .join(' ')} ]`
                 : '')
+            })
           })
-        })
 
-        return Observable.fromPromise(
-          forkJoin(
-            ...dependencies.map(dep => {
-              return ctx[dep]
+          return concat(forkJoin(...dependencies.map(dep => ctx[dep])), Observable.of(0))
+            .flatMap(() => action())
+            .do(null, null, () => {
+              task.title += ' - done!'
+              ctx[name].next('done')
+              ctx[name].complete()
             })
-          )
-            .toPromise()
-            .then(result => {
-              return action().toPromise().then(() => {
-                task.title += ' - done!'
-                ctx[name].next('done')
-                return ctx[name].complete()
-              })
-            })
-        )
+        }
       }
-    })),
-    { concurrent: true, renderer: 'default' }
-  ).run(context)
-}
+      )),
+      { concurrent: true, renderer: 'default' }
+    ).run(context)
+  }
 
-const expandDependencies = task => {
-  const { dependencies } = normalizedInput[task]
-  return Array.prototype.concat(
-    ...dependencies.map(
-      dep =>
-        (normalizedInput[dep].action
-          ? { ...normalizedInput[dep], name: dep }
-          : expandDependencies(dep))
+  expandDependencies (task) {
+    const { dependencies } = this.input[task]
+    return Array.prototype.concat(
+      ...dependencies.map(
+        dep =>
+          (this.input[dep].action
+            ? { ...this.input[dep], name: dep }
+            : this.expandDependencies(dep))
+      )
     )
-  )
+  }
 }
 
-module.exports = { build }
+module.exports = { Tusk }
--- runfile.js
+++ runfile.js
@@ -1,10 +1,5 @@
-const Listr = require('listr')
 const { exec } = require('./lib/exec')
-const { Subject } = require('rxjs/Subject')
-const { Observable } = require('rxjs/Observable')
-const { forkJoin } = require('rxjs/observable/forkJoin')
-const { concat } = require('rxjs/observable/concat')
-const { yellow, green } = require('chalk')
+const { Tusk } = require('./')
 
 const sleep = d => () => exec('sleep', [d.toString()])
 const sleep5 = sleep(5)
@@ -35,84 +30,8 @@ const input = {
   }
 }
 
-const normalizeInput = input =>
-  Object.entries(input).reduce(
-    (a, [key, value]) => ({
-      ...a,
-      [key]: {
-        dependencies: Array.isArray(value)
-          ? value
-          : value.dependencies ? value.dependencies : [],
-        action: typeof value === 'function'
-          ? value
-          : value.action ? value.action : null
-      }
-    }),
-    {}
-  )
+const tusk = new Tusk(input)
 
-const normalizedInput = normalizeInput(input)
-
-const build = () => runTask('build')
-const runTask = taskName => {
-  const result = expandDependencies(taskName)
-  const context = result.reduce((acc, { name }) => {
-    const subject = new Subject()
-    subject.completed = false
-    subject.subscribe(null, null, () => (subject.completed = true))
-    return {
-      ...acc,
-      [name]: subject
-    }
-  }, {})
-
-  return new Listr(
-    result.map(({ name, action, dependencies }) => ({
-      title: name +
-        (dependencies.length > 0
-          ? ` [ ${yellow(dependencies.join(' '))} ]`
-          : ''),
-      task: (ctx, task) => {
-        const dependencies$ = dependencies.map(dep => [dep, ctx[dep]])
-        dependencies$.forEach(([dep, dep$]) => {
-          dep$.next(name)
-          dep$.subscribe(null, null, () => {
-            task.title =
-              name +
-              (dependencies.length > 0
-                ? ` [ ${dependencies$
-                  .map(([dep, dep$]) =>
-                    (dep$.completed ? green : yellow)(dep)
-                  )
-                  .join(' ')} ]`
-                : '')
-          })
-        })
-
-        return concat(forkJoin(...dependencies.map(dep => ctx[dep])), Observable.of(0))
-          .flatMap(() => action())
-          .do(null, null, () => {
-            task.title += ' - done!'
-            ctx[name].next('done')
-            ctx[name].complete()
-          })
-      }
-    }
-    )),
-    { concurrent: true, renderer: 'default' }
-  ).run(context)
-}
-
-const expandDependencies = task => {
-  const { dependencies } = normalizedInput[task]
-  return Array.prototype.concat(
-    ...dependencies.map(
-      dep =>
-        (normalizedInput[dep].action
-          ? { ...normalizedInput[dep], name: dep }
-          : expandDependencies(dep))
-    )
-  )
-}
+const build = () => tusk.run('build')
 
 module.exports = { build }
